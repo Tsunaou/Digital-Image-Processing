908c5ce768878853f51dc44f4b049d851711a7f0
Young <895254752@qq.com>
2019-03-18 22:31:27 +0800

    HSV 算法

--- Lab/lab1/code/test_histeq.m
+++ Lab/lab1/code/test_histeq.m
@@ -1,12 +1,19 @@
 %test histeq
-% I = imread('../asset/wiki1.jpg');
+I = imread('../asset/wiki1.jpg'); 
 % I = imread('../asset/example.png');
 % I = imread('../asset/color.jpg');
-I = imread('../asset/gray.jpg');
+% I = imread('../asset/gray.jpg');
+% I = imread('../asset/xin.png');
+
 [J] = Histogram_equalization(I);
+[HSV] = HSV_equalization(I);
 [S] = Hiseq_standard(I);
 
+    subplot(2,2,1);imshow(I);hold on;title('原图','FontSize',12);
+    subplot(2,2,2);imshow(J);hold on;title('基于RGB直方图均衡化','FontSize',12);
+    subplot(2,2,3);imshow(HSV);hold on;title('基于HSV函数直方图均衡化','FontSize',12);
+    subplot(2,2,4);imshow(S);hold on;title('库函数','FontSize',12);
 %直方图对比
 % Histogram_display(I,J,S);
 %图片对比
-Image_display(I,J,S);
+% Image_display(I,J,S);
