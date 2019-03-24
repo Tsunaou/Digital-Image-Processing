e242f21042393053d35bf9977fdf0225e43013a0
Young <895254752@qq.com>
2019-03-24 15:47:26 +0800

    报告

--- Lab/lab1/code/Histogram_display.m
+++ Lab/lab1/code/Histogram_display.m
@@ -1,20 +1,23 @@
-function [] = Histogram_display(IMG_Origin,IMG_Mine,IMG_Func)
+function [] = Histogram_display(IMG_Origin,IMG_Mine,IMG_A,IMG_B)
     dem = length(size(IMG_Origin));
     switch dem
         case 2
             subplot(2,2,1);imhist(IMG_Origin);hold on;title('原图','FontSize',12);
             subplot(2,2,2);imhist(IMG_Mine);hold on;title('我的函数','FontSize',12);
-            subplot(2,2,3);imhist(IMG_Func);hold on;title('库函数','FontSize',12);
+            subplot(2,2,3);imhist(IMG_A);hold on;title('库函数','FontSize',12);
         case 3
-            subplot(3,3,1);imhist(IMG_Origin(:,:,1));hold on;title('原图 R','FontSize',12);
-            subplot(3,3,2);imhist(IMG_Origin(:,:,2));hold on;title('原图 G','FontSize',12);
-            subplot(3,3,3);imhist(IMG_Origin(:,:,3));hold on;title('原图 B','FontSize',12);
-            subplot(3,3,4);imhist(IMG_Mine(:,:,1));hold on;title('我的函数 R','FontSize',12);
-            subplot(3,3,5);imhist(IMG_Mine(:,:,2));hold on;title('我的函数 G','FontSize',12);
-            subplot(3,3,6);imhist(IMG_Mine(:,:,3));hold on;title('我的函数 B','FontSize',12);
-            subplot(3,3,7);imhist(IMG_Func(:,:,1));hold on;title('库函数 R','FontSize',12);
-            subplot(3,3,8);imhist(IMG_Func(:,:,2));hold on;title('库函数 G','FontSize',12);
-            subplot(3,3,9);imhist(IMG_Func(:,:,3));hold on;title('库函数 B','FontSize',12);
+            subplot(4,3,1);imhist(IMG_Origin(:,:,1));hold on;title('原图 R','FontSize',12);
+            subplot(4,3,2);imhist(IMG_Origin(:,:,2));hold on;title('原图 G','FontSize',12);
+            subplot(4,3,3);imhist(IMG_Origin(:,:,3));hold on;title('原图 B','FontSize',12);
+            subplot(4,3,4);imhist(IMG_Mine(:,:,1));hold on;title('RGB法 R','FontSize',12);
+            subplot(4,3,5);imhist(IMG_Mine(:,:,2));hold on;title('RGB法 G','FontSize',12);
+            subplot(4,3,6);imhist(IMG_Mine(:,:,3));hold on;title('RGB法 B','FontSize',12);
+            subplot(4,3,7);imhist(IMG_A(:,:,1));hold on;title('HSV R','FontSize',12);
+            subplot(4,3,8);imhist(IMG_A(:,:,2));hold on;title('HSV G','FontSize',12);
+            subplot(4,3,9);imhist(IMG_A(:,:,3));hold on;title('HSV B','FontSize',12);
+            subplot(4,3,10);imhist(IMG_B(:,:,1));hold on;title('HSI R','FontSize',12);
+            subplot(4,3,11);imhist(IMG_B(:,:,2));hold on;title('HSI G','FontSize',12);
+            subplot(4,3,12);imhist(IMG_B(:,:,3));hold on;title('HSI B','FontSize',12);
     end  
 end
 
--- Lab/lab1/code/hist_equal.m
+++ Lab/lab1/code/hist_equal.m
@@ -6,12 +6,12 @@ function [output2] = hist_equal(input_channel)
 %0.观察到color.jpg 读入的值是uint8的矩阵，为了保证处理的精度，先将输入通道的精度提高到double
 [input_channel] = im2double(input_channel);
 %1.对图像基础数据计算
-[Height,Width] = size(input_channel); 
-[Counts,Value] = imhist(input_channel);
+[Height,Width] = size(input_channel);   %图像的长宽
+[Counts,Value] = imhist(input_channel); %Value为0-255的灰度值，Counts为每个灰度值对应的像素个数
 
 %2.找到CDF_Min
-CDF_Map = find(Counts~=0);  %指示非0值的
-CDF_Min = Counts(min(CDF_Map));
+CDF_Map = find(Counts~=0);  %指示非0值的下标
+CDF_Min = Counts(min(CDF_Map)); %分布函数中最小的非零值
 %3 计算CDF从而直方图均衡化
 [n,tmp] = size(CDF_Map);
 [L,tmp] = size(Value);
@@ -19,11 +19,11 @@ display("L="+L);
 CDF = 0;
 new_Value = 0;
 for i=1:n
-    CDF = CDF + Counts(CDF_Map(i)); 
+    CDF = CDF + Counts(CDF_Map(i)); %累积分布直方图的值
     den = (Height*Width-CDF_Min);
-    den(den==0) = eps;
-    new_Value = ((CDF-CDF_Min)*(L-1))/den;
-    input_channel(input_channel==Value(CDF_Map(i))) = new_Value;
+    den(den==0) = eps;  %防止由于除零而产生的噪点      
+    new_Value = ((CDF-CDF_Min)*(L-1))/den;  %原灰度值通过累积分布函数的概率密度均衡化后得到的新值
+    input_channel(input_channel==Value(CDF_Map(i))) = new_Value;    %将原图中与原灰度值一样的像素点均衡化赋值
 end
     output2 = input_channel;
 end
