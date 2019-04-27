4c62f7ffb2937c59e05895c10b08e9b9bdcc894b
Young <895254752@qq.com>
2019-04-27 20:23:30 +0800

    Canny

---
+++ Lab/lab2/code/README.md
@@ -0,0 +1,20 @@
+# 数字图像处理 实验2
+
+161220096 欧阳鸿荣
+
+
+
+## 一、文件架构
+
+| 文件名             | 功能         |
+| ------------------ | ------------ |
+| **edge_test**      | 测试入口     |
+| my_sobel           | 索贝尔算子   |
+| my_prewitt         | Prewiit算子  |
+| my_roberts         | Roberts算子  |
+| my_marr            | Marr方法     |
+| my_canny           | Canny方法    |
+| my_laplacian       | 拉普拉斯算子 |
+|                    | 高斯滤波器   |
+| **my_edgelinking** | 边缘连接     |
+
--- Lab/lab2/code/edge_test.m
+++ Lab/lab2/code/edge_test.m
@@ -7,31 +7,37 @@
 %detection, but you just need do edge linking for rubberband_cap.png.
 clc; clear all;
 % Load the test image
-imgTest = im2double(imread('../asset/image/rubberband_cap.png'));
-% imgTest = im2double(imread('../asset/image/5.jpg'));
+% imgTest = im2double(imread('../asset/image/rubberband_cap.png'));
+imgTest = im2double(imread('../asset/image/5.jpg'));
 % imgTest = im2double(imread('../asset/image/a.jpg'));
+
 imgTestGray = rgb2gray(imgTest);
 % figure; clf;imshow(imgTestGray);title('原灰度图')
 
 %now call your function my_edge, you can use matlab edge function to see
 % %the last result as a reference first
 img_edge = edge(imgTestGray);
+% 1.库函数效果
 img_edge_original = edge(imgTestGray);
-img_edge_sobel = my_sobel(imgTestGray);
-img_edge_prewitt = my_prewitt(imgTestGray);
-img_edge_roberts = my_roberts(imgTestGray);
-img_edge_laplacian = my_laplacian(imgTestGray);
-img_edge_marr = my_marr(imgTestGray);
-img_edge_canny = my_canny(imgTestGray);
-
 figure;clf;imshow(img_edge_original);title('库函数效果')
-figure;clf;imshow(img_edge_sobel);title('Sobel效果')
-figure;clf;imshow(img_edge_marr);title('Marr效果')
-figure;clf;imshow(img_edge_canny);title('Canny效果')
+% % 2.sobel算子
+% img_edge_sobel = my_sobel(imgTestGray);
+% figure;clf;imshow(img_edge_sobel);title('Sobel效果')
+% % 3.prewitt算子
+% img_edge_prewitt = my_prewitt(imgTestGray);
 % figure;clf;imshow(img_edge_prewitt);title('Prewitt效果')
+% % 4.roberts算子
+% img_edge_roberts = my_roberts(imgTestGray);
 % figure;clf;imshow(img_edge_roberts);title('Roberts效果')
+% % 5.marr方法
+% img_edge_laplacian = my_laplacian(imgTestGray);
+% img_edge_marr = my_marr(imgTestGray);
+% figure;clf;imshow(img_edge_marr);title('Marr效果')
+% 6.Canny方法
+img_edge_canny = my_canny(imgTestGray);
+figure;clf;imshow(img_edge_canny);title('Canny效果')
 
-% 出于一些特殊原因，先不考虑Laplacian
+% 出于效果原因，先不考虑Laplacian
 % figure;clf;imshow(img_edge_laplacian);title('Laplacian效果')
 
 % subplot(2,3,1);imshow(imgTestGray);hold on;title('原图','FontSize',12);
@@ -50,7 +56,9 @@ figure;clf;imshow(img_edge_canny);title('Canny效果')
 %now call your function my_edgelinking, you can use matlab bwtraceboundary 
 %function to see the last result as a reference first. please trace as many 
 %different object boundaries as you can, and choose different start edge points.
-% Bxpc = bwtraceboundary(img_edge, [197, 329], 'N');
-% Bxpc = my_edgelinking(img_edge, 197, 329);
-% hold on
+% Bxpc = bwtraceboundary(img_edge, [47, 94], 'N');
+% Bxpc = my_edgelinking(img_edge, 94 , 47);
+% imshow(Bxpc);title('边缘追踪','FontSize',12);
+% hold on;
 % plot(Bxpc(:,2), Bxpc(:,1), 'w', 'LineWidth', 1);
+% title('边缘追踪','FontSize',12);
