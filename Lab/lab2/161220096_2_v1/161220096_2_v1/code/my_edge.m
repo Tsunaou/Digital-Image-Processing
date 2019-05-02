c0081743224456e1cf0f37d9169b803f41c3e25e
Young <895254752@qq.com>
2019-05-02 14:43:58 +0800

    报告

--- Lab/lab2/161220096_2_v1/161220096_2_v1/code/my_edge.m
+++ Lab/lab2/161220096_2_v1/161220096_2_v1/code/my_edge.m
@@ -1,4 +1,4 @@
-function output = my_edge(input_image)
+function output = my_edge(input_image,type)
 %in this function, you should finish the edge detection utility.
 %the input parameter is a matrix of a gray image
 %the output parameter is a matrix contains the edge index using 0 and 1
@@ -6,5 +6,19 @@ function output = my_edge(input_image)
 %image
 %you can use different methods to complete the edge detection function
 %the better the final result and the more methods you have used, you will get higher scores
+switch type
+    case 'r'
+        output = my_roberts(input_image);
+    case 'p'
+        output = my_prewitt(input_image);
+    case 's'
+        output = my_sobel(input_image);
+    case 'm'
+        output = my_marr(input_image,4);
+    case 'c'
+        output = my_canny(input_image);
+    otherwise
+        output = my_sobel(input_image);
+end
 
-output = my_sobel(input_image);
+        
