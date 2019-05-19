55e1d4e82894d8a1eef0e239aac54fa80bea6fe8
Young <895254752@qq.com>
2019-05-19 17:09:51 +0800

    基本功能

--- Lab/lab3/161220096_3_v1/code/test_digit.m
+++ Lab/lab3/161220096_3_v1/code/test_digit.m
@@ -1,15 +1,10 @@
 clc; clear all;
-imgInput = imread('../asset/image/test5.jpg'); 
+imgInput = imread('../asset/image/test2.jpg'); 
 imgInput = im2bw(imgInput);
-[m,n,dim] = size(imgInput);
-I = imgInput;
-H = fspecial('unsharp');
-sharpened = imfilter(I,H,'replicate');
-imshow(sharpened);
-digitOutput = my_digit(sharpened);
-display(digitOutput.Words)
 
-function [i,j]= getIndex(index,m,n)
-    i = mod(index,m);
-    j = rem(index,n);
-end
+[m,n] = size(imgInput);
+I = imgInput;
+digitOutput = my_operator(I);
+display(digitOutput)
+
+
